be142ceee14a6115dbbb22682096ea0726211044
Reset <92635343+DanniRen@users.noreply.github.com>
2022-12-28 21:01:03 +0800

    Update directoryManage.cpp

--- directoryManage.cpp
+++ directoryManage.cpp
@@ -12,7 +12,37 @@ void DirectoryManage::CreateRootDirectory() {
 	rootDir->canWrite = 0;//根目录不可写
 	cout<<"创建根目录成功！\n";
 }
-//新建目录：新建一个空目录
+
+//tree显示当前目录的树形结构
+void DirectoryManage::tree(Directory* curDir, int level)
+{
+	if (curDir == workDir) {
+		cout << workDir->name<<"\n";
+	}
+	for (int i = 0; i < level; i++) {
+		cout << "|      ";
+	}
+	if(curDir!=workDir) cout << "|--- " << curDir->name;
+	if (curDir->firstChildDirectory) {
+		Directory* ptr = curDir->firstChildDirectory;
+		while (ptr) {
+			tree(ptr, level + 1);
+			ptr = ptr->nextDirectory;
+		}
+	}
+	if (curDir->firstChildFile) {
+		DirectoryEntry* p = curDir->firstChildFile;
+		while (p) {
+			for (int i = 0; i < level; i++) {
+				cout << "|      ";
+			}
+			cout << "|--- " << p->file.name;
+			p = p->nextFile;
+		}
+	}
+}
+
+//md新建目录：新建一个空目录
 void DirectoryManage::MakeDir(string name) {
 	Directory* newDir = new Directory();
 	newDir->name = name;
@@ -47,14 +77,161 @@ void DirectoryManage::MakeDir(string name) {
 	}
 	cout << "新目录"<<name<<"创建成功！\n";
 }
-//删除目录：删除目录及目录中的文件
-void DirectoryManage::DeleteDir(string name) {
 
+//cd切换目录
+void DirectoryManage::cdDir(string path)
+{
+	if (path == "..") {//返回父目录
+		if (workDir->parentDirectory) workDir = workDir->parentDirectory;
+		else cout << "当前目录已经是根目录\n";
+		return;
+	}
+	else {//切换到当前目录某一路径下的子目录中
+		queue<string> Path;
+		string curPath="";
+		for (int i = 0; i < path.length(); i++) {
+			if (path[i] != '\\' || path[i] != '/') {
+				curPath += path[i];
+			}
+			else {
+				Path.push(curPath);
+				curPath = "";
+			}
+		}
+		if (path.length() != 0 && Path.empty()) {
+			Path.push(path);
+		}
+		queue<Directory*> q;
+		q.push(workDir);
+		while (!q.empty()) {
+			Directory* p = q.front();
+			q.pop();
+			if (p->firstChildDirectory) {
+				Directory* ptr = p->firstChildDirectory;
+				if (ptr->name == Path.front()) {
+					Path.pop();
+					if (Path.empty()) {
+						workDir = ptr;
+						return;
+					}
+					else {
+						q.push(ptr);
+					}
+				}
+				while (ptr->nextDirectory) {
+					ptr = ptr->nextDirectory;
+					if (ptr->name == Path.front()) {
+						Path.pop();
+						if (Path.empty()) {
+							workDir = ptr;
+							return;
+						}
+						else {
+							q.push(ptr);
+						}
+					}
+				}
+			}
+		}
+	}
+	cout << "路径输入有误！\n";
+}
+
+//rd删除目录：删除当前目录中的子目录
+void DirectoryManage::rd(string name) {
+	queue<Directory*> q;
+	q.push(workDir);
+	while (!q.empty()) {
+		Directory* p = q.front();
+		q.pop();
+		if (p->name == name) {
+			if (workDir->firstChildDirectory == p) {
+				workDir->firstChildDirectory = p->nextDirectory;
+				p->nextDirectory->frontDirectory = NULL;
+				DeleteDir(p);
+			}
+			else {
+				Directory* ptr = workDir->firstChildDirectory;
+				while (ptr->nextDirectory) {
+					ptr = ptr->nextDirectory;
+					if (ptr == p) {
+						p->frontDirectory->nextDirectory = p->nextDirectory;
+						if (p->nextDirectory) p->nextDirectory->frontDirectory = p->frontDirectory;
+						DeleteDir(p);
+					}
+				}
+			}
+			cout << "目录'" << name << "'删除成功！\n";
+			return;
+		}
+		if (p->firstChildDirectory) {
+			p = p->firstChildDirectory;
+			q.push(p);
+			while (p->nextDirectory) {
+				p = p->nextDirectory;
+				q.push(p);
+			}
+		}
+	}
+	cout << "不存在'" << name << "'目录\n";
+}
+//删除目录中所有的文件及文件夹
+void DirectoryManage::DeleteDir(Directory* p)
+{
+	if (p) {
+		if (p->firstChildDirectory) {
+			Directory* ptr = p->firstChildDirectory;
+			while (ptr->nextDirectory) {
+				Directory* q = ptr->nextDirectory;
+				DeleteDir(ptr);
+				ptr = q;
+			}
+			DeleteDir(ptr);
+		}
+		if (p->firstChildFile) {
+			DirectoryEntry* ptr = p->firstChildFile;
+			while (ptr->nextFile) {
+				DirectoryEntry* q = ptr->nextFile;
+				delete(ptr);
+				ptr = q;
+			}
+		}
+		delete(p);
+		p = NULL;
+	}
+}
+
+
+//dir命令
+void DirectoryManage::dir()
+{
+	cout << "请选择功能：\n"
+		<< "1.显示当前目录下所有文件夹和文件\n"
+		<< "2.显示当前目录下某文件的信息\n"
+		<< "3.显示当前目录下某一子目录的信息\n";
+	int sel;
+	cin >> sel;
+	if (sel == 1) {
+		ListAll();
+	}
+	else if (sel == 2) {
+		cout << "请输入文件名:";
+		string name;
+		cin >> name;
+		dirFile(name);
+	}
+	else if (sel == 3) {
+		cout << "请输入目录的相对路径：";
+		string dir_path="";
+		cin >> dir_path;
+		dirPath(dir_path);
+	}
 }
 //显示当前目录下所有文件夹和文件
 void DirectoryManage::ListAll() {
+	cout << "----------------------------------------------------------------\n";
 	queue<Directory*> q;
-	q.push(rootDir);
+	q.push(workDir);
 	while (!q.empty()) {
 		Directory* p = q.front();
 		q.pop();
@@ -85,44 +262,181 @@ void DirectoryManage::ListAll() {
 			}
 		}
 	}
+	cout << "----------------------------------------------------------------\n";
 }
-//修改目录：重命名
-void DirectoryManage::ModifyDir(string name,string newName) {
-	if (name == newName) return;
-	queue<Directory*> q;
-	q.push(rootDir);
-	while (!q.empty()) {
-		Directory* ptr = q.front();
-		q.pop();
-		if (ptr->name == name&&ptr->canWrite!=1&&ptr->canWrite!=user.userRight) {//权限判断
-			cout << "用户权限不足，无法修改！\n";
-			return;
-		}
-		else if(ptr->name==name){
-			//重名判断
-			Directory* p = ptr->parentDirectory->firstChildDirectory;
-			while (p) {
-				if (p->name == newName) {
-					cout << "该目录名称已存在，无法重命名该目录！\n";
-					return;
+//显示当前目录下某文件的信息
+void DirectoryManage::dirFile(string name) {
+	if (workDir->firstChildFile) {
+		DirectoryEntry* ptr = workDir->firstChildFile;
+		while (ptr) {
+			if (ptr->file.name == name) {
+				cout << "创建时间：" << ctime(&ptr->file.create_time)<<"\n";
+				cout << "文件名：" << ptr->file.name << "\n";
+				cout << "文件大小" << ptr->file.size<<"\n";
+				cout << "文件属性";
+				if (ptr->file.canRead == user.userRight || ptr->file.canRead == 1) {
+					cout << "可读  ";
 				}
-				p = p->nextDirectory;
+				else cout << "不可读  ";
+				if (ptr->file.canWrite == user.userRight || ptr->file.canWrite == 1) {
+					cout << "可写\n";
+				}
+				else cout << "不可写\n";
 			}
-			ptr->name = newName;
-			return;
+			ptr = ptr->nextFile;
 		}
-		if (ptr->firstChildDirectory) {
-			ptr = ptr->firstChildDirectory;
-			q.push(ptr);
-			while (ptr->nextDirectory) {
-				ptr = ptr->nextDirectory;
-				q.push(ptr);
-			}
+	}
+	cout << "不存在该文件！\n";
+}
+//显示某子目录的信息
+void DirectoryManage::ListDir(Directory* p)
+{
+	if (p->canRead != user.userRight && p->canRead != 1) {
+		cout << "不好意思，您没有权限读取该目录的信息！\n";
+		return;
+	}
+	if (p->firstChildDirectory || p->firstChildFile) {
+		GetPath(p);
+		cout << path << "下子目录及文件\n";
+	}
+	else {
+		cout << "当前目录为空目录！\n";
+		return;
+	}
+	if (p->firstChildDirectory) {
+		p = p->firstChildDirectory;
+		cout << '\t' << p->name << '\t';
+		cout << "<DIR>\n";
+		while (p->nextDirectory) {
+			p = p->nextDirectory;
+			cout << '\t' << p->name << '\t';
+			cout << "<DIR>\n";
+		}
+	}
+	if (p->firstChildFile) {
+		DirectoryEntry* ptr = p->firstChildFile;
+		cout << '\t' << ptr->file.name << '\t';
+		cout << "<FILE>\n";
+		while (ptr->nextFile) {
+			ptr = ptr->nextFile;
+			cout << '\t' << ptr->file.name << '\t';
+			cout << "<FILE>\n";
 		}
 	}
 }
+//显示相对路径下某子目录的信息
+void DirectoryManage::dirPath(string path)
+{
+	queue<string> Path;
+	string curPath = "";
+	for (int i = 0; i < path.length(); i++) {
+		if (path[i] != '\\' || path[i] != '/') {
+			curPath += path[i];
+		}
+		else {
+			Path.push(curPath);
+			curPath = "";
+		}
+	}
+	if (path.length() != 0 && Path.empty()) {
+		Path.push(path);
+	}
+	queue<Directory*> q;
+	q.push(workDir);
+	while (!q.empty()) {
+		Directory* p = q.front();
+		q.pop();
+		if (Path.empty()) {
+			ListDir(p);
+			return;
+		}
+		if (p->firstChildDirectory) {
+			Directory* ptr = p->firstChildDirectory;
+			if (ptr->name == Path.front()) {
+				Path.pop();
+				q.push(ptr);
+				continue;
+			}
+			while (ptr->nextDirectory) {
+				ptr = ptr->nextDirectory;
+				if (ptr->name == Path.front()) {
+					Path.pop();
+					q.push(ptr);
+					continue;
+				}
+			}
+		}
+	}
+	cout << "路径输入有误或不存在该目录！\n";
+}
+
+//ren命令
+void DirectoryManage::ren(string name, string newName)
+{
+	//重命名目录
+	if (workDir->firstChildDirectory) {
+		Directory* p = workDir->firstChildDirectory;
+		if (p->name == name) {
+			RenameDir(p, newName);
+			return;
+		}
+	}
+	//重命名文件
+	if (workDir->firstChildFile) {
+		DirectoryEntry* q = workDir->firstChildFile;
+		if (q->file.name == name) {
+			RenameFile(q, newName);
+			return;
+		}
+	}
+}
+//重命名目录
+void DirectoryManage::RenameDir(Directory* ptr,string newName) {
+	//权限判断
+	if (ptr->canWrite!=1&&ptr->canWrite!=user.userRight) {
+		cout << "用户权限不足，无法修改！\n";
+		return;
+	}
+	if(workDir->firstChildDirectory){
+		//重命名判断
+		Directory* p = workDir->firstChildDirectory;
+		while (p) {
+			if (p != ptr&&p->name == newName) {
+				cout << "该目录名称已存在，无法重命名该目录！\n";
+				return;
+			}
+			p = p->nextDirectory;
+		}
+		ptr->name = newName;
+		cout << "目录名修改成功！新目录名为" << ptr->name << "\n";
+		return;
+	}
+}
+//重命名文件
+void DirectoryManage::RenameFile(DirectoryEntry* ptr,string newName) {
+	//权限判断
+	if (ptr->file.canWrite != 1 && ptr->file.canWrite != user.userRight) {
+		cout << "用户权限不足，无法修改！\n";
+		return;
+	}
+	if(workDir->firstChildFile) {
+		//重名判断
+		DirectoryEntry* p = workDir->firstChildFile;
+		while (p) {
+			if (p!=ptr&&p->file.name == newName) {
+				cout << "该文件名称已存在，无法重命名该文件！\n";
+				return;
+			}
+			p = p->nextFile;
+		}
+		ptr->file.name = newName;
+		cout << "文件名修改成功！新文件名为" << ptr->file.name << "\n";
+		return;
+	}
+}
+
 //获取当前文件夹的路径
-void DirectoryManage::GetPath(Directory* curDir) {
+string DirectoryManage::GetPath(Directory* curDir) {
 	path = "";
 	stack<Directory*> q;
 	q.push(curDir);
@@ -136,8 +450,10 @@ void DirectoryManage::GetPath(Directory* curDir) {
 		path += p->name;
 		if (!q.empty()) path += "\\";
 	}
+	return path;
 }
-//创建文件：在此方法中输入数据大小、数据信息、存储目录、文件名等，然后调用数据生成线程来创建文件。
+
+//create创建文件
 void DirectoryManage::CreateFile(string name) {
 	//检查文件是否重名
 	DirectoryEntry* p = workDir->firstChildFile;
@@ -148,89 +464,196 @@ void DirectoryManage::CreateFile(string name) {
 		}
 		p = p->nextFile;
 	}
+	//创建文件
 	File* newFile=new File();
 	newFile->name = name;
-	newFile->size = newFile->context.length();
+	cout << "请输入文件内容：\n";
 	getline(cin,newFile->context);
-	time_t create_time;
-	time(&create_time);
-	newFile->structure = "text";
+	newFile->size = newFile->context.length();
+	time(&newFile->create_time);//创建文件的时间
+	newFile->structure = "text";//文件结构默认文本文件
+	newFile->owner = user.name;
 	newFile->canRead = user.userRight;
 	newFile->canWrite = user.userRight;
-	//执行数据生成线程
-	//线程：调用磁盘分配的函数、调用文件目录项函数
+	//执行数据生成线程,传递的参数有File newFile
+	//thread t(data_generation_thread); // 运行到这边就开一个新的线程执行data_generation_thread函数
+	//t.join();
 
 }
 //为文件建立目录项
-void DirectoryManage::CreateDirEntry(File file,string owner, time_t create_time, int first_block) {
+void DirectoryManage::CreateDirEntry(File file, int first_block) {
 	DirectoryEntry* newDirEntry = new DirectoryEntry();
 	newDirEntry->file = file;
-	newDirEntry->owner = owner;
-	newDirEntry->create_time = create_time;
 	newDirEntry->first_block = first_block;
 	newDirEntry->frontFile = workDir->lastChildFile;
 	workDir->lastChildFile->nextFile = newDirEntry;
 	workDir->lastChildFile = newDirEntry;
 	newDirEntry->nextFile = NULL;
 	newDirEntry->parentDirectory = workDir;
-
 }
-//删除文件：删除文件的目录项
+
+
+//del删除文件
 void DirectoryManage::DeleteFile(string name) {
-
-}
-//读文件：可读文件时才能读
-void DirectoryManage::ReadFile(string name) {
-
-}
-//写文件：可写文件时才能写
-void DirectoryManage::WriteFile(string name) {
-
-}
-//修改文件：重命名或修改文件权限
-void DirectoryManage::ModifyFile(string name) {
-	string newName;
-	cin >> newName;
-	if (name == newName) return;
-	queue<Directory*> q;
-	q.push(rootDir);
-	while (!q.empty()) {
-		Directory* ptr = q.front();
-		q.pop();
-		if (ptr->name == name && ptr->canWrite != 1 && ptr->canWrite != user.userRight) {//权限判断
-			cout << "用户权限不足，无法修改！\n";
-			return;
-		}
-		else if (ptr->name == name) {
-			//重名判断
-			Directory* p = ptr->parentDirectory->firstChildDirectory;
-			while (p) {
-				if (p->name == newName) {
-					cout << "该文件名称已存在，无法重命名该文件！\n";
-					return;
+	if (workDir->firstChildFile) {
+		DirectoryEntry* ptr = workDir->firstChildFile;
+		while (ptr) {
+			if (ptr->file.name == name) {
+				if (ptr == workDir->firstChildFile) {
+					workDir->firstChildFile = ptr->nextFile;
+					ptr->nextFile->frontFile = NULL;
+					delete ptr;
 				}
-				p = p->nextDirectory;
+				else if (ptr == workDir->lastChildFile) {
+					workDir->lastChildFile = ptr->frontFile;
+					ptr->frontFile->nextFile = ptr->nextFile;
+				}
+				else {
+					ptr->frontFile->nextFile = ptr->nextFile;
+					ptr->nextFile->frontFile = ptr->frontFile;
+					delete ptr;
+				}
+				cout << "文件"<<name<<"删除成功！\n";
+				return;
 			}
-			ptr->name = newName;
-			return;
+			ptr = ptr->nextFile;
 		}
-		if (ptr->firstChildDirectory) {
-			ptr = ptr->firstChildDirectory;
-			q.push(ptr);
+	}
+	cout << "不存在该文件！\n";
+}
+
+//type显示文件内容，可读文件时才能读
+void DirectoryManage::ReadFile(string path) {
+	//读取文件相对路径
+	queue<string> Path;
+	string curPath = "";
+	for (int i = 0; i < path.length(); i++) {
+		if (path[i] != '\\' || path[i] != '/') {
+			curPath += path[i];
+		}
+		else {
+			Path.push(curPath);
+			curPath = "";
+		}
+	}
+	if (path.length() != 0 && Path.empty()) {
+		Path.push(path);
+	}
+
+	//读取文件信息
+	queue<Directory*> q;
+	q.push(workDir);
+	while (!q.empty()) {
+		Directory* p = q.front();
+		q.pop();
+		if (Path.size() == 1) {
+			if (workDir->firstChildFile) {
+				DirectoryEntry* t = workDir->firstChildFile;
+				while (t) {
+					if (t->file.name == Path.front()) {
+						//权限判断
+						if (t->file.canRead != 1 && t->file.canRead != user.userRight) {
+							cout << "用户权限不足，无法查看该文件内容！\n";
+							return;
+						}
+						//输出文件内容
+						cout << t->file.context;
+						//访问内存的线程,传递的参数为File file
+
+						return;
+					}
+					t = t->nextFile;
+				}
+			}
+		}
+		
+		if (p->firstChildDirectory) {
+			Directory* ptr = p->firstChildDirectory;
+			if (ptr->name == Path.front()) {
+				Path.pop();
+				q.push(ptr);
+				
+			}
 			while (ptr->nextDirectory) {
 				ptr = ptr->nextDirectory;
-				q.push(ptr);
+				if (ptr->name == Path.front()) {
+					Path.pop();
+					q.push(ptr);
+				}
 			}
 		}
 	}
 }
-//文件的打开
-void DirectoryManage::OpenFile(string name) {
-	//执行线程，将文件调入内存
-	// 执行线程：调用分配内存块函数
-	//让用户选择读文件还是写文件
-}
-//文件的关闭
-void DirectoryManage::CloseFile(string name) {
 
+//echo写文件，可写文件时才能写
+void DirectoryManage::WriteFile(string name) {
+	if (workDir->firstChildFile) {
+		DirectoryEntry* p = workDir->firstChildFile;
+		while (p) {
+			if (p->file.name == name) {
+				//权限判断
+				if (p->file.canWrite != 1 && p->file.canWrite != user.userRight) {
+					cout << "用户权限不足，无法编写该文件内容！\n";
+					return;
+				}
+				cout << "请输入文件内容：\n";
+				string context;
+				getline(cin, context);
+				p->file.context += context;
+				cout << "写文件成功！\n";
+				//访问内存的线程,传递的参数为File file
+
+				return;
+			}
+			p = p->nextFile;
+		}
+	}
+	cout << "不存在'" << name << "'文件！\n";
+}
+
+//open文件的打开
+void DirectoryManage::OpenFile(string name) {
+	int flag = 0;
+	DirectoryEntry* p = NULL;
+	if (workDir->firstChildFile) {
+		p = workDir->firstChildFile;
+		while (p) {
+			if (p->file.name == name) {
+				flag = 1;
+				break;
+			}
+			p = p->nextFile;
+		}
+	}
+	if (flag == 0) {
+		cout << "不存在'" << name << "'文件\n";
+		return;
+	}
+
+	//执行线程：将文件调入内存,传递的参数为File file
+
+	cout << "打开文件成功！\n";
+}
+//close文件的关闭
+void DirectoryManage::CloseFile(string name) {
+	int flag = 0;
+	DirectoryEntry* p = NULL;
+	if (workDir->firstChildFile) {
+		p = workDir->firstChildFile;
+		while (p) {
+			if (p->file.name == name) {
+				flag = 1;
+				break;
+			}
+			p = p->nextFile;
+		}
+	}
+	if (flag == 0) {
+		cout << "不存在'" << name << "'文件\n";
+		return;
+	}
+
+	//回收内存的线程，将文件从内存中删除,传递的参数为File file
+
+	cout << "关闭文件成功！\n";
 }
