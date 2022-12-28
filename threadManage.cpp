24af98d5e5068772a2046fd845790bb06ebbf760
杜研书 <duyanshu@duyanshudeMacBook-Air.local>
2022-12-28 19:51:33 +0800

    data_generation写了一半

--- threadManage.cpp
+++ threadManage.cpp
@@ -1,6 +1,13 @@
 #include "threadManage.h"
-
-void data_generation_thread()
+void data_generation_thread(File &newFile)
 {
-    // 调用磁盘分配的函数、调用文件目录项函数
+    // 调用磁盘分配的函数,若成功返回1，失败返回-1
+    int status = AllocateBlocks(newFile->name, newFile->size, newFile->context);
+    if (status == 1)
+    {
+        // 成功分配内存块
+        }
+    else
+    {
+    }
 }
--- threadManage.h
+++ threadManage.h
@@ -1,11 +1,16 @@
 #include <thread>
 #include "memoryManage.h"
+#include "directoryManage.h"
+#include "diskManage.h"
+
 using namespace std;
 // Mutex and condition variable for synchronizing access to memory
 std::mutex memory_mutex;
 std::condition_variable memory_cv;
+DirectoryManage directoryManger;
+
 // Function prototypes
-void data_generation_thread();
+void data_generation_thread(File newFile);
 void delete_data_thread();
 void execute_thread();
 void visualize_memory();
