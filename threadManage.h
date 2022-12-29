3cc483c0da64bc315191b6d756b78d0224af9298
Du Yanshu <72860852+duyanshu@users.noreply.github.com>
2022-12-29 22:25:49 +0800

    Update threadManage.h

--- threadManage.h
+++ threadManage.h
@@ -1,18 +1,30 @@
 #include <thread>
-#include "memoryManage.h"
+#include <chrono>
+// #include "memoryManage.h"
 #include "directoryManage.h"
-#include "DiskManager.h"
+// #include "DiskManager.h"
 
 using namespace std;
 // Mutex and condition variable for synchronizing access to memory
-std::mutex memory_mutex;
-std::condition_variable memory_cv;
-DirectoryManage directoryManger;
-DiskManager diskManager;
-MemoryManager memoryManager;
+// std::mutex memory_mutex;
+// std::condition_variable memory_cv;
 
-// Function prototypes
-void data_generation_thread(File newFile);
-void delete_data_thread();
-void execute_thread();
-void visualize_memory();
+// 本类对磁盘管理、目录管理、内存管理的类都进行了实例化，用来调用其他类的方法，其它类不可调用这个类的方法
+class ThreadManager
+{
+public:
+    DirectoryManage DIR;
+    // DiskManager diskManager;
+    // MemoryManager memoryManager;
+    vector<std::thread> threads;
+    // 函数声明
+    ThreadManager(); // 构造函数中完成初始化
+    int initialize();
+    void showMenu();
+    void data_generation_thread(DirectoryManage::File &newFile);
+    void delete_data_thread(string name);
+    void execute_thread();
+    void visualize_memory();
+    void joinAllThreads();
+    int generateNumber(int min, int max);
+};
